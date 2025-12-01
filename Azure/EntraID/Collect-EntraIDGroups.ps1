bdecdf6920079ca071532dfc0fd61f9473891ec4
Maciej Pawiński <maciej.pawinski@op.pl>
2025-12-02 00:05:29 +0100

    Changes: - lovered privileges, - improved Graph modules checkup, - updated array construct, - added user notifications.

--- Azure/EntraID/Collect-EntraIDGroups.ps1
+++ Azure/EntraID/Collect-EntraIDGroups.ps1
@@ -4,34 +4,82 @@
     Autor: Maciej Pawiński
 
 .DESCRIPTION
-    The scripts collect all groups with members and properties, then the result is exported to JSON file.  
+    The scripts collect all groups with members and properties. T result is exported to JSON file.  
 
-.PARAMETER directoryPath
+.PARAMETER OutputDirectory
     Directory in which output file will be saved. If left blank, then output file will be saved in the current directory.
 
 .EXAMPLE
-    .\\CollecGroups.ps1 -directoryPath 'C:\temp'
+    .\\CollecGroups.ps1 -OutputDirectory 'C:\temp'
 #>
 
 param (
-    [string]$directoryPath
+    [string]$OutputDirectory
 )
 
-# Check Graph connection
+$RequiredModules = @(
+    "Microsoft.Graph.Authentication",
+    "Microsoft.Graph.Groups",
+    "Microsoft.Graph.Identity.SignIns",
+    "Microsoft.Graph.Identity.Governance"
+)
+
+Write-Host "Checking required Microsoft Graph modules..."
+
+foreach ($module in $RequiredModules) {
+    if (-not (Get-InstalledModule -Name $module -ErrorAction SilentlyContinue)) {
+        Write-Warning "$module is not installed. Installing..."
+        try {
+            Install-Module -Name $module -Scope CurrentUser -Force -ErrorAction Stop
+            Write-Host "$module installed successfully."
+        }
+        catch {
+            Write-Error "Failed to install $module : $_"
+            exit 1
+        }
+    }
+}
+Write-Host "All modules installed, checking connection to Microosft Graph..."
+#Check Graph connection
 try {
 	if (-not (Get-MgContext)) {
-		Connect-MgGraph -Scopes "Group.Read.All", "User.Read.All","Directory.Read.All"-NoWelcome # Directory.Read.All access is neccessary to read assigned roles to groups
-
+		Connect-MgGraph -Scopes "Group.Read.All", "User.Read.All","RoleManagement.Read.Directory" -NoWelcome
+        Write-Host "Connected to Microsoft Graph..."
 	}
 } catch {
 	Write-Error "Failed to connect to Microsoft Graph: $_"
 	exit
 }
 
-$directoryRoleAssignement = @()
-$directoryRoleAssignement = Get-MgRoleManagementDirectoryRoleAssignment
-$tenantId = (Get-MgOrganization).Id
-# Pagination function
+function Export-Output {
+    [string]$currentDate = Get-Date -Format dd_MM_yyyy
+    $OutputDirectory = if ($OutputDirectory) { [string]::Concat($OutputDirectory, '\') } else { ".\" }
+    $baseFileName = "groups_$currentDate"
+    $extension = ".json"
+    $counter = 0
+
+    do {
+        $fileName = if ($counter -eq 0) { "$baseFileName$extension" } else { [string]::Concat($baseFileName,"_$counter",$extension) }
+        $filePath = [System.IO.Path]::Combine($OutputDirectory, $fileName)
+        $counter++
+    } while (Test-Path $filePath)
+
+    try {
+        $OutputObject = [PSCustomObject]@{
+            TenantId = $TenantId
+            Groups   = $GroupList
+        }
+        $OutputObject | ConvertTo-Json -Depth 5 | Out-File -FilePath $filePath -Encoding UTF8
+        Write-Output "JSON data saved to $filePath"
+    } catch {
+        Write-Error "Failed to save JSON data to $filePath : $_"
+    }
+}
+
+$DirectoryRoleAssignement = @()
+$DirectoryRoleAssignement = Get-MgRoleManagementDirectoryRoleAssignment
+$TenantId = (Get-MgOrganization).Id
+#Pagination function
 function Get-AllPages {
     param (
         [scriptblock]$Command
@@ -51,18 +99,17 @@ function Get-AllPages {
     }
     return $results
 }
-
 function Get-Roles {
     param (
-        [string]$groupId
+        [string]$GroupId
     )
     $roles = $directoryRoleAssignement | Where-Object {$_.PrincipalId -eq $group.Id} 
-    $roles = $roles | Select -Property RoleDefinitionId
+    $roles = $roles | Select-Object -Property RoleDefinitionId
     return $roles
 }
 function Get-LicenseSku {
     param (
-        [string]$groupId
+        [string]$GroupId
     )
 
     $group = Get-MgGroup -GroupId $groupId -Property "AssignedLicenses"
@@ -71,11 +118,12 @@ function Get-LicenseSku {
     return $skuIds
 }
 
-$groups = Get-AllPages -Command { Get-MgGroup -All:$true }
+Write-Host "Collecting groups..."
+$Groups = Get-AllPages -Command { Get-MgGroup -All:$true }
+$GroupList = [System.Collections.Generic.List[PSObject]]::New()
+Write-Host "Collecting groups details..."
 
-$groupList = @()
-
-foreach ($group in $groups) {
+foreach ($group in $Groups) {
     try {
        
         $owners = Get-AllPages -Command { Get-MgGroupOwner -GroupId $group.Id }
@@ -104,44 +152,16 @@ foreach ($group in $groups) {
             AssignedRoles       = Get-Roles -groupId $group.Id
             Members             = $memberDetails
         }
-
-        $groupObject = [PSCustomObject]@{
+        $GroupList.add([PSCustomObject]@{
             Name            = $group.DisplayName
             ObjectId        = $group.Id
             Properties      = $groupProperties
-        }
-
-        $groupList += $groupObject
+        })
     } catch {
         Write-Warning "Failed to process group '$($group.DisplayName)' (ID: $($group.Id)): $_"
         continue
     }
 }
 
-
-$outputObject = [PSCustomObject]@{
-    TenantId = $tenantId
-    Groups   = $groupList
-}
-
-
-[string]$today = Get-Date -Format dd_MM_yyyy
-
-
-$directoryPath = if ($directoryPath) { [string]::Concat($directoryPath, '\') } else { ".\" }
-$baseFileName = "groups_$today"
-$extension = ".json"
-$counter = 0
-
-do {
-    $fileName = if ($counter -eq 0) { "$baseFileName$extension" } else { [string]::Concat($baseFileName,"_$counter",$extension) }
-    $filePath = [System.IO.Path]::Combine($directoryPath, $fileName)
-    $counter++
-} while (Test-Path $filePath)
-
-try {
-    $outputObject | ConvertTo-Json -Depth 5 | Out-File -FilePath $filePath -Encoding UTF8
-    Write-Output "JSON data saved to $filePath"
-} catch {
-    Write-Error "Failed to save JSON data to $filePath : $_"
-}
+Export-Output
+Disconnect-MgGraph > $null
