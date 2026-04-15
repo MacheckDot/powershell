18ad9f2c5b024f359cb548a7ffe79638fc8f2f65
Maciej Pawiński <maciej.pawinski@op.pl>
2026-04-15 15:49:06 +0200

    modified:   SPO/Get-UniqePersmissionsOnSite.ps1 updates:  - correctd login option to more secure way

--- SPO/Get-UniqePersmissionsOnSite.ps1
+++ SPO/Get-UniqePersmissionsOnSite.ps1
@@ -1,37 +1,28 @@
-# Load SharePoint Online Management Shell module
 Import-Module Microsoft.Online.SharePoint.PowerShell
+$siteUrl = RRead-Host "Enter SPO Url:"
 
-# Connect to SharePoint Online
-$siteUrl = "https://pernodricard-admin.sharepoint.com/"
+Connect-SPOService -Url $siteUrl -ModernAuth $true -AuthenticationUrl https://login.microsoftonline.com/organizations
 
-Connect-SPOService -Url $siteUrl
 
-# Function to list files and folders recursively
 function Get-FilesAndFolders {
     param (
         [string]$folderUrl
     )
 
-    # Get the items in the folder
     $items = Get-PnPListItem -List "Documents" -FolderServerRelativeUrl $folderUrl
 
     foreach ($item in $items) {
         if ($item.FileSystemObjectType -eq "File") {
-            # It's a file
             Write-Host "File: $($item.FieldValues['FileLeafRef'])"
         } elseif ($item.FileSystemObjectType -eq "Folder") {
-            # It's a folder
             $folderName = $item.FieldValues['FileLeafRef']
             Write-Host "Folder: $folderName"
-            # Call the function recursively for the subfolder
             List-FilesAndFolders -folderUrl "$folderUrl/$folderName"
         }
     }
 }
 
-# Specify the root folder (change to your desired folder)
 $rootFolderUrl = "/sites/yoursite/Shared Documents"
 Get-FilesAndFolders -folderUrl $rootFolderUrl
 
-# Disconnect from SharePoint Online
 Disconnect-SPOService
